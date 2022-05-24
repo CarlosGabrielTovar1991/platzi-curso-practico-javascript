d11c22a85b3afdf931511b56798bcd0e7045322b
Carlos Gabriel Tovar <carlosgabrieltovar1991@gmail.com>
2022-05-24 08:42:52 +0200

    Taller #2 - cupones de descuento.

--- precios/descuentos.js
+++ precios/descuentos.js
@@ -24,3 +24,15 @@ function onClickButtonPriceDiscount() {
   const precioConDescuento = calcularDescuento(inputPriceValue, inputDiscountPriceValue);
   resultPrice.innerText = `El precio con descuento a pagar es de $${precioConDescuento}`;
 }
+
+function onClickButtonPriceDiscountCoupon() {
+  const inputPrice = document.getElementById("InputPriceCoupon");
+  const inputPriceValue = inputPrice.value
+  const inputDiscount = document.getElementById("InputDiscountCoupon");
+  const inputDiscountPriceValue = inputDiscount.value;
+  const resultPriceCoupon = document.getElementById("ResultPriceCoupon");
+
+  const precioConDescuento = calcularDescuento(inputPriceValue, inputDiscountPriceValue);
+  const messageCoupon = inputDiscountPriceValue <= 0 ? "No has seleccionado ningún cupón" : `Has seleccionado un cupón del ${inputDiscountPriceValue}% de descuento`;
+  resultPriceCoupon.innerText = `Tu precio es de ${inputPriceValue}\n${messageCoupon}\nEl precio con descuento a pagar es de $${precioConDescuento}`;
+}
