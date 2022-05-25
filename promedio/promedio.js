34f1681885633a4255c13522215ebb745a9285c1
Carlos Gabriel Tovar <carlosgabrieltovar1991@gmail.com>
2022-05-25 11:31:55 +0200

    Taller #3 - promedio, mediana y moda. Cambiar ciclo por funcion de array. Lista de valores estatica eliminada. Lista de valores como parametro de función. Pequeño manejo de errores.

--- promedio/promedio.js
+++ promedio/promedio.js
@@ -1,16 +1,24 @@
-const listaUno = [
-  100,
-  200,
-  300,
-  500
-];
-
-function calcularPromedio() {
-  let sumaElementos = 0;
-  let mediaAritmetica = 0;
-  const cantidadElementos = listaUno.length;
-  for(let iterador = 0; iterador < cantidadElementos; iterador++) {
-    sumaElementos += listaUno[iterador];
+function calcularMediaAritmetica(lista = null) {
+  if (!Array.isArray(lista)) {
+    console.error("No se puede calcular la media aritmética: el parametro recibido no es una lista");
+    return 0;
   }
-  mediaAritmetica = sumaElementos / cantidadElementos;
+  const cantidadElementos = lista.length;
+  if (cantidadElementos <= 0) {
+    console.error("No se puede calcular la media aritmética: la lista de elementos está vacía.")
+    return 0;
+  }
+  /* Solución con ciclo for */
+  // let sumaElementos = 0;
+  // for(let iterador = 0; iterador < cantidadElementos; iterador++) {
+  //   sumaElementos += lista[iterador];
+  // }
+
+  /* Solución con función reduce de array */
+  const sumaElementos = lista.reduce(
+    function (valorAcumulado = 0, elementoActual) {
+      return (valorAcumulado + elementoActual)
+    }
+  );
+  return sumaElementos / cantidadElementos;
 }
